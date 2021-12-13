9f93b4525d558bc845a20bd5cf02f3df68b480a0
Chiel Kunkels <chiel@kunkels.me>
2021-12-13 21:57:52 +0100

    chore: move domain out of repo

--- terraform/domain.tf
+++ terraform/domain.tf
@@ -1,15 +1,10 @@
-resource "digitalocean_domain" "kunkels_me" {
-  name = "kunkels.me"
-}
-
-# subdomains
 resource "digitalocean_record" "kunkels_me" {
   for_each = {
     "@"   = var.ingress_ip
     "www" = var.ingress_ip
   }
 
-  domain = digitalocean_domain.kunkels_me.id
+  domain = "kunkels.me"
   type   = "A"
   name   = each.key
   ttl    = 3600
